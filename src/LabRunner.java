735d1143cd60c419886a59805b3214b95fdeef73
Sneeky Stryder <ifazilov3584@bths.edu>
2022-10-11 19:52:41 -0400

    lab done

--- src/LabRunner.java
+++ src/LabRunner.java
@@ -1,17 +1,9 @@
 public class LabRunner {
     public static void main(String[] args) {
-        System.out.println("Use \"\\n\"\nfor a new line");
-        System.out.println("Brooklyn Tech\n29 Ft. Greene\nBrooklyn");
-        System.out.println("An \"i\"\nfor\nan eye");
-        System.out.println("The string \"\"\nis an empty string.");
-        System.out.println("The escape sequence for \\ is \\\\");
-        System.out.println("The escape sequence for \" is \\\"");
-        System.out.println("/A\\V/A\\\n\\V/A\\V/");
-        System.out.println("My favorite punctuation mark is \"\\\"");
-        System.out.println("Now I know my\n A\n  B\n   C’s");
-        System.out.println("¯\\_(\"/)_/¯");
-        System.out.println("Dear Jen,\nYou got an \"A\"!\n \nSincerely,\nThe teacher");
-        System.out.println(" * *\n* * *\n * *");
-        System.out.println("Hello there!\n\"\\__/\"");
+        Student student = new Student("Abby", "Smith", 2023);
+        student.addTestScore(95);
+        student.addTestScore(98);
+        System.out.println(student.getStudentInfo());
+
     }
 }
